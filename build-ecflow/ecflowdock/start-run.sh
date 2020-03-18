71683868d79d306fbb3c7d0e1d185a8031274fde
Espen Myrland <espen@myrland.space>
2020-03-18 18:16:55 +0100

    map $HOME

--- build-ecflow/ecflowdock/start-run.sh
+++ build-ecflow/ecflowdock/start-run.sh
@@ -1,3 +1,3 @@
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix   -i -t myggen/ecflow5-bionic:0.1 /bin/bash
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -ti -v /etc/passwd:/etc/passwd -v $HOME:$HOME  -u `id -u`:`id -g` myggen/ecflow5-bionic:0.1 /bin/bash
 #docker run --net=host --env="DISPLAY"  -i -t myggen/ecflow5-bionic:0.1 /bin/bash
 #docker run --env="DISPLAY"  -i -t myggen/ecflow5-bionic:0.1 /bin/bash
