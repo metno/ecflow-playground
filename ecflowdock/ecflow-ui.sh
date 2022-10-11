24ca280263353910e301463340b1e4d7b1574f9b
Espen Myrland <espenm@met.no>
2022-10-11 15:19:24 +0200

    ecflow-ui.sh: use 5.9.0

--- ecflowdock/ecflow-ui.sh
+++ ecflowdock/ecflow-ui.sh
@@ -1,6 +1,6 @@
 #docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -ti -v /etc/passwd:/etc/passwd -v $HOME:$HOME  -u `id -u`:`id -g` myggen/ecflow5-bionic:firsttry /opt/ecflow-5.2.3/bin/ecflow_ui
 
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -d --rm -v /etc/passwd:/etc/passwd -v $HOME:$HOME  -u `id -u`:`id -g` myggen/ecflow5-jammy:0.1 /opt/ecflow-5.8.4/bin/ecflow_ui
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -d --rm -v /etc/passwd:/etc/passwd -v $HOME:$HOME  -u `id -u`:`id -g` myggen/ecflow5-jammy:0.1 /opt/ecflow-5.9.0/bin/ecflow_ui
 
 #docker run --net=host --env="DISPLAY"  -i -t myggen/ecflow5-bionic:0.1 /bin/bash
 #docker run --env="DISPLAY"  -i -t myggen/ecflow5-bionic:0.1 /bin/bash
